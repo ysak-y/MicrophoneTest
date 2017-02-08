f8d98afc25ed1d11889bf94e9d67fba562d7ed03
yoshiaki-yamada <yoshiaki-yamada@cookpad.com>
2017-02-08 23:25:56 +0900

    First commit

---
+++ MicrophoneTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MicrophoneTest
+//
+//  Created by yoshiaki-yamada on 2017/02/08.
+//  Copyright © 2017年 yoshiaki-yamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
